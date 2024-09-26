8c432d2b60532e2673857d0d7720ee49d02467e6
A23droid <pillaiaditya2310@gmail.com>
2024-09-26 18:49:20 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,21 @@
+def is_Palindrome(string):
+    if string == "":
+        return "Not Palindrome"
+    
+    elif string == string[::-1]:
+        return "Palindrome"
+    
+    else:
+        new_str = ""
+        for i in string.lower():
+            if i.isalpha():
+                new_str += i
+        if new_str == new_str[::-1]:
+            return "Palindrome"
+        else:
+            return "Not Palindrome"
+    
+s = input("Enter: ")
+
+print(is_Palindrome(s.lower()))
+
